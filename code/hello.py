b7f661720dc3f0c9da48a23682464a2d3f3bfde1
Bansidhar P.M <bansidharpm@gmail.com>
2020-03-30 19:22:55 +0530

    plot example added

--- code/hello.py
+++ code/hello.py
@@ -1,3 +1,8 @@
 # Try out a simple python file checkin.
-print("Hello Git world") 
-print("hello Git world - Again")
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+x = np.linspace(0,20,100)
+plt.plot(x,np.sin(x))
+plt.show()
