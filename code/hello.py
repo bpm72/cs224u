3762a1f43f87ff5309abe253b7aab924dd4fdc98
Bansidhar P.M <bansidharpm@gmail.com>
2020-04-20 16:27:45 +0530

    removed hello.py test file

--- code/hello.py
+++
@@ -1,9 +0,0 @@
-# Try out a simple python file checkin.
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-
-# Add a sinusiodal signal plot
-x = np.linspace(0,20,100)
-plt.plot(x,np.sin(x))
-plt.show()
